8125ce77d0b9ce0789c4b0da31e1d917433eb4e6
GH_LEE <dlrjsgml42@naver.com>
2021-09-10 00:12:50 +0900

    feat: Charger 클래스와 Response DTO 클래스 구현
    - Charger 클래스 구현
    - ChargerResponseDto 클래스 구현. 생성자를 통해 값 주입받음

--- src/main/java/com/evinfo/domain/charger/domain/Charger.java
+++ src/main/java/com/evinfo/domain/charger/domain/Charger.java
@@ -1,8 +1,12 @@
 package com.evinfo.domain.charger.domain;
 
+<<<<<<< HEAD
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+=======
+import lombok.*;
+>>>>>>> feat: Charger 클래스와 Response DTO 클래스 구현
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
@@ -11,6 +15,11 @@ import javax.validation.constraints.NotNull;
 // TODO: 2021/09/09 추후 시간나면 validation + enum exception에 대한 도메인 테스트 구현
 @Entity
 @Getter
+<<<<<<< HEAD
+=======
+@Builder
+@AllArgsConstructor
+>>>>>>> feat: Charger 클래스와 Response DTO 클래스 구현
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Charger {
     @Id
@@ -30,7 +39,10 @@ public class Charger {
     @NotBlank
     private String address;
 
+<<<<<<< HEAD
     @NotBlank
+=======
+>>>>>>> feat: Charger 클래스와 Response DTO 클래스 구현
     private String location;
 
     @NotBlank
