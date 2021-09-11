badac68f748741d4883f721cf84cdff580e79fe9
GH_LEE <dlrjsgml42@naver.com>
2021-09-12 01:39:19 +0900

    feat: 문서화를 위한 RestDocs 코드 작성 및 gradle과 api.docs 설정
    - gradle에 restdocs 설정 및 테스트 결과 출력 경로 지정
    - api.docs 구현하여 chargers/gets에 대한 문서화 및 공통 문서화 내용 삽입
    - Documentation 클래스 구현. 컨트롤러 테스트에서 상속하는 클래스로, 문서화 관련 내용 삽입
    - ChargerControllerTest에 문서화 로직 반영

--- src/test/java/com/evinfo/domain/controller/ChargerControllerTest.java
+++ src/test/java/com/evinfo/domain/controller/ChargerControllerTest.java
@@ -2,6 +2,7 @@ package com.evinfo.domain.controller;
 
 import com.evinfo.domain.charger.domain.Charger;
 import com.evinfo.domain.charger.service.ChargerClient;
+import com.evinfo.domain.controller.docs.Documentation;
 import com.evinfo.utils.ChargerGenerator;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,6 +11,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -18,13 +21,17 @@ import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
-public class ChargerControllerTest {
+public class ChargerControllerTest extends Documentation {
     private static final String API = "/api";
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
@@ -34,9 +41,10 @@ public class ChargerControllerTest {
     private MockMvc mockMvc;
 
     @BeforeEach
-    void setUp(WebApplicationContext context) {
+    void setUp(WebApplicationContext context, RestDocumentationContextProvider restDocumentation) {
         this.mockMvc = MockMvcBuilders
                 .webAppContextSetup(context)
+                .apply(documentationConfiguration(restDocumentation))
                 .build();
     }
 
@@ -54,6 +62,24 @@ public class ChargerControllerTest {
                 .andExpect(jsonPath("$[1].id").value(chargerResponses.get(1).getId()))
                 .andExpect(jsonPath("$[0].stationName").value(chargerResponses.get(0).getStationName()))
                 .andExpect(jsonPath("$[1].stationName").value(chargerResponses.get(1).getStationName()))
-                .andDo(print());
+                .andDo(print())
+                .andDo(document("chargers/gets",
+                        getDocumentRequest(),
+                        getDocumentResponse(),
+                        responseFields(
+                                fieldWithPath("[]").type(JsonFieldType.ARRAY).description("전체 충전기의 목록"),
+                                fieldWithPath("[].id").type(JsonFieldType.NUMBER).description("충전기의 ID"),
+                                fieldWithPath("[].stationName").type(JsonFieldType.STRING).description("충전기 충전소의 이름"),
+                                fieldWithPath("[].stationId").type(JsonFieldType.STRING).description("충전기 충전소의 ID"),
+                                fieldWithPath("[].chargerType").type(JsonFieldType.STRING).description("충전기의 타입"),
+                                fieldWithPath("[].address").type(JsonFieldType.STRING).description("충전기의 주소"),
+                                fieldWithPath("[].location").type(JsonFieldType.STRING).description("충전기의 세부 주소"),
+                                fieldWithPath("[].useTime").type(JsonFieldType.STRING).description("충전기의 사용 가능 시간"),
+                                fieldWithPath("[].lat").type(JsonFieldType.NUMBER).description("충전기의 위도"),
+                                fieldWithPath("[].lng").type(JsonFieldType.NUMBER).description("충전기의 경도"),
+                                fieldWithPath("[].callNumber").type(JsonFieldType.STRING).description("충전기 충전소의 전화번호"),
+                                fieldWithPath("[].chargerStat").type(JsonFieldType.STRING).description("충전기의 상태")
+                        )
+                ));
     }
 }
