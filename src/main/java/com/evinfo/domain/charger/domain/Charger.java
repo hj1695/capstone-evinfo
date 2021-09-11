2cd6dc22e94d7bbd6e47250fe3a1dd1623d3113c
GH_LEE <dlrjsgml42@naver.com>
2021-09-11 13:18:31 +0900

    refactor: rebase 과정에서 생긴 코드 변경 확인

--- src/main/java/com/evinfo/domain/charger/domain/Charger.java
+++ src/main/java/com/evinfo/domain/charger/domain/Charger.java
@@ -1,12 +1,6 @@
 package com.evinfo.domain.charger.domain;
 
-<<<<<<< HEAD
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-=======
 import lombok.*;
->>>>>>> feat: Charger 클래스와 Response DTO 클래스 구현
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
@@ -15,11 +9,8 @@ import javax.validation.constraints.NotNull;
 // TODO: 2021/09/09 추후 시간나면 validation + enum exception에 대한 도메인 테스트 구현
 @Entity
 @Getter
-<<<<<<< HEAD
-=======
 @Builder
 @AllArgsConstructor
->>>>>>> feat: Charger 클래스와 Response DTO 클래스 구현
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Charger {
     @Id
@@ -39,10 +30,6 @@ public class Charger {
     @NotBlank
     private String address;
 
-<<<<<<< HEAD
-    @NotBlank
-=======
->>>>>>> feat: Charger 클래스와 Response DTO 클래스 구현
     private String location;
 
     @NotBlank
