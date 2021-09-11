cca0351c8f7acff33b8157777d9c99d164ce990c
GH_LEE <dlrjsgml42@naver.com>
2021-09-11 13:38:48 +0900

    feat: 로깅 작업을 위한 LogInterceptor 구현, Config 작성
    - LogInterceptor를 구현하여 다양한 요청에 대해 인터셉터 역할을 하도록 함
    - WebMvcConfig를 만들고, 모든 요청에 대해 LogInterceptor를 거치도록 함
    - 현재는 LogInterceptor에서 로그 발생시키는 중
    - .gitignore에서 logs를 추가하여 제외할 수 있도록 설정

---
+++ src/main/java/com/evinfo/global/config/WebMvcConfig.java
@@ -0,0 +1,24 @@
+package com.evinfo.global.config;
+
+import com.evinfo.global.logger.LogInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    private final LogInterceptor logInterceptor;
+
+    public WebMvcConfig(LogInterceptor logInterceptor) {
+        this.logInterceptor = logInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor).addPathPatterns("/**")
+                .excludePathPatterns("/css/**")
+                .excludePathPatterns("/js/**")
+                .excludePathPatterns("/index.html");
+    }
+}
+
