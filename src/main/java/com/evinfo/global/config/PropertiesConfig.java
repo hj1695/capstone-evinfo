35634b66eacef38fe623e143afb182ff6eaf5836
GH_LEE <dlrjsgml42@naver.com>
2021-09-13 10:04:28 +0900

    feat: PropertiesConfig 추가 및 yml import, 테스트 추가
    - PropertiesConfig에서 RestProperties를 사용 가능하도록 등록.
    - application.yml에서 rest.yml을 참조
    - RestPropertiesTest에서 RestProperties가 올바르게 동작하는지 테스트

--- src/main/java/com/evinfo/global/config/PropertiesConfig.java
+++ src/main/java/com/evinfo/global/config/PropertiesConfig.java
@@ -9,5 +9,4 @@ import org.springframework.context.annotation.Configuration;
 @EnableConfigurationProperties(RestProperties.class)
 @Configuration
 public class PropertiesConfig {
-    // TODO: 2021/09/13 yml 파일 가져오는거 여기서 말고 다른데서 처리하기, 그리고 enum docs test에 ChargerType이랑 그 Stat넣어주기
 }
