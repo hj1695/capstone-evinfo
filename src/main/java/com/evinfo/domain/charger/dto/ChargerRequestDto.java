fdf57c117e79ef1ac76c9cce892f6f7070fb1a8f
GH_LEE <dlrjsgml42@naver.com>
2021-09-21 16:34:11 +0900

    feat: ChargerService에서 거리 기준 정렬 후 페이징된 개수 반환하는 로직 구현
    - ChargerService에서 getChargers를 호출하면, 입력받은 위도/경도 기준 제일 가까운 size개의 원소를 찾아 반환함.
    - 위의 연산을 수행하기 위해 private 메소드 추가
    - ChargerRequestDto에 생성자 추가, 테스트 위해 추가함
    - ChargerController에서 인자 전달 로직 추가

--- src/main/java/com/evinfo/domain/charger/dto/ChargerRequestDto.java
+++ src/main/java/com/evinfo/domain/charger/dto/ChargerRequestDto.java
@@ -1,6 +1,8 @@
 package com.evinfo.domain.charger.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.Max;
@@ -8,6 +10,8 @@ import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ChargerRequestDto {
     @NotNull
     Double latitude;
--- src/main/java/com/evinfo/domain/charger/service/ChargerService.java
+++ src/main/java/com/evinfo/domain/charger/service/ChargerService.java
@@ -1,13 +1,17 @@
 package com.evinfo.domain.charger.service;
 
+import com.evinfo.domain.charger.domain.Charger;
+import com.evinfo.domain.charger.dto.ChargerRequestDto;
 import com.evinfo.domain.charger.dto.ChargerResponseDto;
 import com.evinfo.domain.charger.repository.ChargerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Map;
+import java.util.TreeMap;
 
 @RequiredArgsConstructor
 @Service
@@ -15,10 +19,30 @@ public class ChargerService {
     private final ChargerRepository chargerRepository;
 
     @Transactional(readOnly = true)
-    public List<ChargerResponseDto> getChargers() {
-        return chargerRepository.findAll()
-                .stream()
-                .map(ChargerResponseDto::new)
-                .collect(Collectors.toList());
+    public List<ChargerResponseDto> getChargers(ChargerRequestDto request) {
+        List<Charger> chargers = chargerRepository.findAll();
+        Map<Double, Charger> distanceSortedMap = getDistanceSortedMap(request, chargers);
+        List<ChargerResponseDto> responses = new ArrayList<>();
+
+        for (var entry : distanceSortedMap.entrySet()) {
+            responses.add(new ChargerResponseDto(entry.getValue()));
+            if (responses.size() >= request.getSize()) break;
+        }
+
+        return responses;
+    }
+
+    private Map<Double, Charger> getDistanceSortedMap(ChargerRequestDto request, List<Charger> chargers) {
+        Map<Double, Charger> distanceMap = new TreeMap<>();
+        for (var charger : chargers) {
+            Double distance = calculateDistance(charger, request);
+            distanceMap.put(distance, charger);
+        }
+        return distanceMap;
+    }
+
+    private Double calculateDistance(Charger charger, ChargerRequestDto request) {
+        return Math.sqrt(Math.pow(charger.getLng() - request.getLongitude(), 2.0)
+                + Math.pow(charger.getLat() - request.getLatitude(), 2.0));
     }
 }
