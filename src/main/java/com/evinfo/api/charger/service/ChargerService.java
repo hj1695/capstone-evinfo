ef6d25ba8700e56d60aadc353fe19a78816b07bd
GH_LEE <dlrjsgml42@naver.com>
2021-09-24 01:18:17 +0900

    refactor: 최적화를 위해 ChargerService의 로직 개선
    - 기존의 TreeMap을 사용하는 정렬 대신 stream에 정렬을 수행 후 subset을 구하는 식으로 값 반환

--- src/main/java/com/evinfo/api/charger/service/ChargerService.java
+++ src/main/java/com/evinfo/api/charger/service/ChargerService.java
@@ -8,11 +8,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
@@ -22,21 +19,16 @@ public class ChargerService {
 
     @Transactional(readOnly = true)
     public List<ChargerResponseDto> getChargers(ChargerRequestDto request) {
-        Map<Double, ChargerResponseDto> distanceSortedMap = chargerRepository.findAll()
+        List<ChargerResponseDto> responses = chargerRepository.findAll()
                 .stream()
                 .map(charger -> {
                     Double distance = calculateDistance(charger, request);
                     return new ChargerResponseDto(charger, distance);
                 })
-                .collect(Collectors.toMap(ChargerResponseDto::getDistance, Function.identity(), (o1, o2) -> o1, TreeMap::new));
-        List<ChargerResponseDto> responses = new ArrayList<>();
+                .sorted(Comparator.comparing(ChargerResponseDto::getDistance))
+                .collect(Collectors.toList());
 
-        for (var entry : distanceSortedMap.entrySet()) {
-            responses.add(entry.getValue());
-            if (responses.size() >= request.getSize()) break;
-        }
-
-        return responses;
+        return responses.subList(0, request.getSize().intValue());
     }
 
     private Double calculateDistance(Charger charger, ChargerRequestDto request) {
