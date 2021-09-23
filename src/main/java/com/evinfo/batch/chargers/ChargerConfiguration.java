0108792030e353dbdbdc38e0e02a4347feeb5520
GH_LEE <dlrjsgml42@naver.com>
2021-09-24 01:19:26 +0900

    feat: ChargerConfiguration Batch 작성 및 ChargerClient 변경
    - ChargerConfiguration에 기본적인 Batch 프로그램 작성
    - ChargerClient의 저장 로직 삭제
    - ChargerController에 변경사항 반영

--- src/main/java/com/evinfo/batch/chargers/ChargerConfiguration.java
+++ src/main/java/com/evinfo/batch/chargers/ChargerConfiguration.java
@@ -1,15 +1,25 @@
 package com.evinfo.batch.chargers;
 
+import com.evinfo.api.charger.dto.client.ChargerClientResponseDto;
+import com.evinfo.api.charger.service.ChargerClient;
+import com.evinfo.domain.charger.Charger;
+import com.evinfo.global.common.RestProperties;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.database.JpaItemWriter;
+import org.springframework.batch.item.support.ListItemReader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManagerFactory;
 
 @Configuration
 @Slf4j(topic = "BATCH_FILE_LOGGER")
@@ -17,6 +27,9 @@ import org.springframework.context.annotation.Configuration;
 public class ChargerConfiguration {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
+    private final ChargerClient chargerClient;
+    private final RestProperties restProperties;
+    private final EntityManagerFactory entityManagerFactory;
 
     @Bean
     public Job chargerInitJob() {
@@ -27,11 +40,28 @@ public class ChargerConfiguration {
     }
 
     @Bean
+    @JobScope
     public Step chargerInitStep() {
         return stepBuilderFactory.get("chargerInitStep")
-                .tasklet((contribution, chunkContext) -> {
-                    log.info("hello spring batch");
-                    return RepeatStatus.FINISHED;
-                }).build();
+                .<ChargerClientResponseDto, Charger>chunk(restProperties.getEvinfoChunk().intValue())
+                .reader(chargerInitReader())
+                .processor(chargerInitProcessor())
+                .writer(chargerInitWriter())
+                .build();
+    }
+
+    private ItemReader<ChargerClientResponseDto> chargerInitReader() {
+        return new ListItemReader<>(chargerClient.fetchChargers());
+    }
+
+    private ItemProcessor<ChargerClientResponseDto, Charger> chargerInitProcessor() {
+        return ChargerClientResponseDto::getCharger;
+    }
+
+    private JpaItemWriter<Charger> chargerInitWriter() {
+        JpaItemWriter<Charger> jpaItemWriter = new JpaItemWriter<>();
+        jpaItemWriter.setEntityManagerFactory(entityManagerFactory);
+
+        return jpaItemWriter;
     }
 }
