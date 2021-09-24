c0637820878c6ae9e9331efb7f1bc87bab78b667
GH_LEE <dlrjsgml42@naver.com>
2021-09-24 15:01:37 +0900

    feat: 서비스의 profile을 test, local로 나누고 해당 profile 테스트에 적용
    - local에서는 profile 인자로 local을 주고, mysql 바탕의 관리가 가능하도록 설정
    - test에서는 h2 in-memory db로 테스트 수행하도록 하고 flyway 적용 해제함. @SpringBootTest가 붙은 테스트들에 test profile 설정

--- src/test/java/com/evinfo/batch/ChargerConfigurationTest.java
+++ src/test/java/com/evinfo/batch/ChargerConfigurationTest.java
@@ -15,9 +15,11 @@ import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.mockito.Mockito.when;
 
+@ActiveProfiles(profiles = "test")
 @SpringBatchTest
 @SpringBootTest
 class ChargerConfigurationTest {
