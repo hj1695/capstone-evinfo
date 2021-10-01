1a02d74242993baf7a11347f86bfa942c1e02349
GH_LEE <dlrjsgml42@naver.com>
2021-10-01 16:45:12 +0900

    feat: ChargerService의 거리 로직 수정, kilometer로 값 반환
    - ChargerService의 calculateDistance 로직을 haversine formula 기반으로 수정
    - 결과값을 미터 기준으로 반올림하고, 키로미터 단위로 변환하여 반환(즉, 소수점 셋째 자리까지 반환)

--- src/main/java/com/evinfo/api/charger/service/ChargerService.java
+++ src/main/java/com/evinfo/api/charger/service/ChargerService.java
@@ -16,6 +16,8 @@ import java.util.stream.Collectors;
 @Service
 public class ChargerService {
     private final ChargerRepository chargerRepository;
+    private static final Double RADIUS = 6371.0;
+    private static final Double KILOMETER_PER_METER = 1000.0;
 
     @Transactional(readOnly = true)
     public List<ChargerResponseDto> getChargers(ChargerRequestDto request) {
@@ -27,12 +29,24 @@ public class ChargerService {
                 })
                 .sorted(Comparator.comparing(ChargerResponseDto::getDistance))
                 .collect(Collectors.toList());
-
+        if (request.getSize() > responses.size())
+            return responses;
         return responses.subList(0, request.getSize().intValue());
     }
 
     private Double calculateDistance(Charger charger, ChargerRequestDto request) {
-        return Math.sqrt(Math.pow(charger.getLongitude() - request.getLongitude(), 2.0)
-                + Math.pow(charger.getLatitude() - request.getLatitude(), 2.0));
+        double toRadian = Math.PI / 180.0;
+
+        double deltaLatitude = Math.abs(charger.getLatitude() - request.getLatitude()) * toRadian;
+        double deltaLongitude = Math.abs(charger.getLongitude() - request.getLongitude()) * toRadian;
+
+        double sinDeltaLatitude = Math.sin(deltaLatitude / 2.0);
+        double sinDeltaLongitude = Math.sin(deltaLongitude / 2.0);
+        double squareRoot = Math.sqrt(
+                sinDeltaLatitude * sinDeltaLatitude +
+                        Math.cos(charger.getLatitude() * toRadian) * Math.cos(charger.getLatitude() * toRadian) * sinDeltaLongitude * sinDeltaLongitude);
+        double distance = 2 * RADIUS * Math.asin(squareRoot);
+
+        return Math.round(distance * KILOMETER_PER_METER) / KILOMETER_PER_METER;
     }
 }
