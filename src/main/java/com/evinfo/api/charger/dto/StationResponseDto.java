6328a5b2a8be6a3031b0005b235f9d31af09907f
GH_LEE <dlrjsgml42@naver.com>
2021-10-06 10:37:02 +0900

    feat: Station과 Charger에 맞는 ResponseDto 구현
    - StationResponseDto 구현. 충전소 정보와 거리 정보, 충전기 목록 제공
    - ChargerResponseDto가 Charger에 대한 정보만 가지도록 수정
    - ChargerClientResponseDto에서 Charger와 Station 모두 변환하도록 수정

---
+++ src/main/java/com/evinfo/api/charger/dto/StationResponseDto.java
@@ -0,0 +1,37 @@
+package com.evinfo.api.charger.dto;
+
+import com.evinfo.domain.charger.Station;
+import lombok.Getter;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+public class StationResponseDto {
+    private final String stationName;
+    private final String stationId;
+    private final String address;
+    private final String location;
+    private final String useTime;
+    private final Double latitude;
+    private final Double longitude;
+    private final String callNumber;
+    private final Double distance;
+    private final List<ChargerResponseDto> chargers;
+
+    public StationResponseDto(final Station station, final Double distance) {
+        this.stationName = station.getStationName();
+        this.stationId = station.getStationId();
+        this.address = station.getAddress();
+        this.location = station.getLocation();
+        this.useTime = station.getUseTime();
+        this.latitude = station.getLatitude();
+        this.longitude = station.getLongitude();
+        this.callNumber = station.getCallNumber();
+        this.distance = distance;
+        this.chargers = station.getChargers()
+                .stream()
+                .map(ChargerResponseDto::new)
+                .collect(Collectors.toList());
+    }
+}
