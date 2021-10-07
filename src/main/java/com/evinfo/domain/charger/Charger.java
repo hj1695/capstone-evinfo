6210c4697da095156e2ac140d5de554920f2e3cb
GH_LEE <dlrjsgml42@naver.com>
2021-10-07 17:43:19 +0900

    refactor: 도메인 코드의 복합키 오류 수정 및 생성자 변경
    - 도메인 코드에서 복합키의 필드명을 잘못 선언하여 오류 발생, 수정
    - Charger와 Station의 빌더 생성자를 직접 코드로 선언. lombok으로 AllArgsConstructor 사용시 예기치 못한 오류 가능
    - Station 생성시 내부 필드 중 충전기 목록(chargers)은 빈 리스트로 생성하도록 함. 그렇지 않은 경우 builder로 생성하다가 chargers를 누락했을 때, addChargers 메소드를 사용하면 NPE가 발생함

--- src/main/java/com/evinfo/domain/charger/Charger.java
+++ src/main/java/com/evinfo/domain/charger/Charger.java
@@ -1,6 +1,9 @@
 package com.evinfo.domain.charger;
 
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
@@ -9,11 +12,14 @@ import javax.validation.constraints.NotNull;
 // TODO: 2021/09/09 추후 시간나면 validation + enum exception에 대한 도메인 테스트 구현
 @Entity
 @Getter
-@Builder
-@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @IdClass(ChargerCompositeId.class)
 public class Charger {
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "STATION_ID")
+    private Station station;
+
     @Id
     @NotBlank
     private String chargerId;
@@ -26,10 +32,13 @@ public class Charger {
     @NotNull
     private ChargerStat chargerStat;
 
-    @Id
-    @ManyToOne
-    @JoinColumn(name = "STATION_ID")
-    private Station station;
+    @Builder
+    public Charger(Station station, String chargerId, ChargerType chargerType, ChargerStat chargerStat) {
+        this.station = station;
+        this.chargerId = chargerId;
+        this.chargerType = chargerType;
+        this.chargerStat = chargerStat;
+    }
 
     public void updateChargerStat(final ChargerStat chargerStat) {
         this.chargerStat = chargerStat;
--- src/main/java/com/evinfo/domain/charger/Station.java
+++ src/main/java/com/evinfo/domain/charger/Station.java
@@ -1,6 +1,9 @@
 package com.evinfo.domain.charger;
 
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -8,12 +11,11 @@ import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Getter
-@Builder
-@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Station {
     @Id
@@ -43,6 +45,19 @@ public class Station {
 
     @OneToMany(mappedBy = "station")
     private List<Charger> chargers;
+
+    @Builder
+    public Station(String stationId, String stationName, String address, String location, Double latitude, Double longitude, String callNumber, String useTime) {
+        this.stationId = stationId;
+        this.stationName = stationName;
+        this.address = address;
+        this.location = location;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.callNumber = callNumber;
+        this.useTime = useTime;
+        this.chargers = new ArrayList<>();
+    }
 
     public void addCharger(final Charger charger) {
         this.chargers.add(charger);
