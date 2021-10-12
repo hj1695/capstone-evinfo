56e80365a4defbb265b2e55ee3438c3dc2bfcf70
GH_LEE <dlrjsgml42@naver.com>
2021-10-12 12:53:02 +0900

    refactor: jobLauncher에서 Job을 실행할 때 RunID 설정 추가
    - 기존 스케쥴러에서 Job을 실행하는 로직은 파리미터 정보를 주지 않음. 그래서 runIdCrementer가 작동하지 않음
    - 관련하여 jobLauncher 수정

--- src/main/java/com/evinfo/batch/charger/scheduler/ChargerScheduler.java
+++ src/main/java/com/evinfo/batch/charger/scheduler/ChargerScheduler.java
@@ -3,8 +3,9 @@ package com.evinfo.batch.charger.scheduler;
 import com.evinfo.batch.charger.ChargerInitConfiguration;
 import com.evinfo.batch.charger.ChargerUpdateConfiguration;
 import lombok.RequiredArgsConstructor;
-import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.explore.JobExplorer;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
@@ -18,14 +19,14 @@ public class ChargerScheduler {
     private final ChargerInitConfiguration chargerInitConfiguration;
     private final ChargerUpdateConfiguration chargerUpdateConfiguration;
     private final JobLauncher jobLauncher;
+    private final JobExplorer jobExplorer;
 
-    @Scheduled(cron = "0 0/10 * * * *")
+    @Scheduled(cron = "0 0/2 * * * *")
     public void runUpdateJob() throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
-        jobLauncher.run(chargerUpdateConfiguration.chargerUpdateJob(), new JobParameters());
+        jobLauncher.run(
+                chargerUpdateConfiguration.chargerUpdateJob(), new JobParametersBuilder(jobExplorer)
+                        .getNextJobParameters(chargerUpdateConfiguration.chargerUpdateJob())
+                        .toJobParameters()
+        );
     }
-
-//    @Scheduled(cron = "0 0 03 * * *")
-//    public void runInitJob() throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
-//        jobLauncher.run(chargerInitConfiguration.chargerInitJob(), new JobParameters());
-//    }
 }
