4ebbe3d4718caad1772ee88b4745bce46c655e51
GH_LEE <dlrjsgml42@naver.com>
2021-10-14 19:24:36 +0900

    feat: init batch에 변경사항 반영 및 올바른 수행 확인
    - ChargerInitJob의 쿼리에 Station과 Charger의 필드 추가 반영
    - ChargerCreateRequestDto에 output 필드 추가

--- src/main/java/com/evinfo/batch/charger/dto/ChargerCreateRequestDto.java
+++ src/main/java/com/evinfo/batch/charger/dto/ChargerCreateRequestDto.java
@@ -9,11 +9,13 @@ public class ChargerCreateRequestDto {
     private final String chargerId;
     private final String chargerStat;
     private final String chargerType;
+    private final Long output;
 
     public ChargerCreateRequestDto(Charger charger) {
         this.stationId = charger.getStation().getStationId();
         this.chargerId = charger.getChargerId();
         this.chargerStat = charger.getChargerStat().name();
         this.chargerType = charger.getChargerType().name();
+        this.output = charger.getOutput();
     }
 }
