4d5a2070af15104550e1f54a8fd02f3664be742f
GH_LEE <dlrjsgml42@naver.com>
2021-10-27 09:22:40 +0900

    feat: batch 로직 및 batch용 DTO에 Charger의 변경사항 반영
    - chargerInitJob의 쿼리 코드에 추가된 Charger 필드를 반영
    - chargerUpdateJob의 로직에 새로운 필드에 대한 조건 및 update 코드 추가
    - batch에서 사용되는 DTO 클래스에 새로운 필드 추가

--- src/main/java/com/evinfo/batch/charger/dto/ChargerCreateRequestDto.java
+++ src/main/java/com/evinfo/batch/charger/dto/ChargerCreateRequestDto.java
@@ -3,6 +3,8 @@ package com.evinfo.batch.charger.dto;
 import com.evinfo.domain.charger.Charger;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class ChargerCreateRequestDto {
     private final String stationId;
@@ -11,6 +13,8 @@ public class ChargerCreateRequestDto {
     private final String chargerType;
     private final Long output;
     private final Double price;
+    private final LocalDateTime lastChargeDateTime;
+    private final LocalDateTime startChargeDateTime;
 
     public ChargerCreateRequestDto(Charger charger) {
         this.stationId = charger.getStation().getStationId();
@@ -19,5 +23,7 @@ public class ChargerCreateRequestDto {
         this.chargerType = charger.getChargerType().name();
         this.output = charger.getOutput();
         this.price = charger.getPrice();
+        this.lastChargeDateTime = charger.getLastChargeDateTime();
+        this.startChargeDateTime = charger.getStartChargeDateTime();
     }
 }
--- src/main/java/com/evinfo/batch/charger/dto/ChargerUpdateRequestDto.java
+++ src/main/java/com/evinfo/batch/charger/dto/ChargerUpdateRequestDto.java
@@ -2,9 +2,12 @@ package com.evinfo.batch.charger.dto;
 
 import com.evinfo.api.charger.dto.client.ChargerClientResponseDto;
 import com.evinfo.domain.charger.ChargerStat;
+import com.evinfo.global.utils.DateTimeTranslator;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Builder
 @RequiredArgsConstructor
@@ -13,12 +16,16 @@ public class ChargerUpdateRequestDto {
     private final String stationId;
     private final String chargerId;
     private final ChargerStat chargerStat;
+    private final LocalDateTime lastChargeDateTime;
+    private final LocalDateTime startChargeDateTime;
 
     public static ChargerUpdateRequestDto valueOf(final ChargerClientResponseDto clientResponse) {
         return ChargerUpdateRequestDto.builder()
                 .stationId(clientResponse.getStatId())
                 .chargerId(clientResponse.getChgerId())
                 .chargerStat(ChargerStat.valueOf(clientResponse.getStat()))
+                .lastChargeDateTime(DateTimeTranslator.exchangeLocalDateTime(clientResponse.getLastTedt()))
+                .startChargeDateTime(DateTimeTranslator.exchangeLocalDateTime(clientResponse.getStatUpdDt()))
                 .build();
     }
 }
