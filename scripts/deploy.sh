e732f7b4963c72c4f84e9ebedf61c2354178df47
GH_LEE <dlrjsgml42@naver.com>
2021-11-03 15:12:38 +0900

    hotfix: 전반적인 스크립트 정상화 및 디렉토리 생성

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -31,12 +31,6 @@ echo "> $JAR_NAME 에 실행권한 추가"
 
 chmod +x $JAR_NAME
 
-echo "> Logback 실행을 위한 디렉토리 추가"
-
-sudo rmdir logs
-sudo mkdir logs
-sudo chmod +w logs
-
 echo "> $JAR_NAME 실행"
 
 nohup java -jar -Xms512m -Xmx512m \
