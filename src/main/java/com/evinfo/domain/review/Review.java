990520bee42ee93d3ab18755db452d130d6ea707
GH_LEE <dlrjsgml42@naver.com>
2021-12-07 22:47:16 +0900

    feat: Review 도메인에 생성일자 필드 추가
    - Review 도메인에 자동 생성되는 생성일자 필드 추가
    - 해당 옵션 적용을 위해 JpaAuditingConfig 파일 추가

--- src/main/java/com/evinfo/domain/review/Review.java
+++ src/main/java/com/evinfo/domain/review/Review.java
@@ -5,14 +5,18 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 @AllArgsConstructor
 public class Review {
     @Id
@@ -24,6 +28,9 @@ public class Review {
 
     @NotNull // TODO: 2021/12/03 Double은 Min/Max 체크 안됨. validation할 방법 찾기.
     private Double star;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
 
     @ManyToOne
     @JoinColumn(name = "STATION_ID")
